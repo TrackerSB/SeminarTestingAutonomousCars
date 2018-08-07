48cea3bfe0a3d3f1185a8bf2f4ff1e44c6394bc1
Stefan Huber <junktogo@web.de>
2018-08-07 17:16:11 +0200

    Introduce project config

--- BeamNG/SimpleBeamNG/main.py
+++ BeamNG/SimpleBeamNG/main.py
@@ -4,20 +4,23 @@ from configparser import ConfigParser
 
 import beamngpy
 
-currentConfigSection = 'BeamNG'
+
+class ProjectConfig:
+    currentConfigSection = 'BeamNG'
+    widthOfScreenshot = 800
+    numScreenshots = 10
+
 
 config = ConfigParser()
 dirname = os.path.abspath(os.path.dirname(__file__))  # NOTE When inserting this line into the next, it may fail
 config.read(os.path.join(dirname, '..', 'BeamNG.ini'))
 
-# FIXME First execution does not work
-userpath = config.get(currentConfigSection, 'userpath')
-binary = config.get(currentConfigSection, 'x64_binary')
+userpath = config.get(ProjectConfig.currentConfigSection, 'userpath')
+binary = config.get(ProjectConfig.currentConfigSection, 'x64_binary')
 with beamngpy.beamng.BeamNGPy('localhost', 64256, userpath=userpath,
                               binary=binary, console=True) as bpy:
     bpy.load_scenario('levels/west_coast_usa/scenarios/basic_scenario.json')
-    time.sleep(10)
-    bpy.start_scenario()  # FIXME Why is this called to early?
+    bpy.start_scenario()  # NOTE This method blocks until the restart button is pressed
     bpy.hide_hud()
     bpy.vcontrol({
         'throttle': 1.0,
@@ -25,9 +28,9 @@ with beamngpy.beamng.BeamNGPy('localhost', 64256, userpath=userpath,
         'gear': 1
     })
 
-    outputDir = config.get(currentConfigSection, 'output_dir')
-    for i in range(10):
-        vstate = bpy.get_vstate(800)
+    outputDir = config.get(ProjectConfig.currentConfigSection, 'output_dir')
+    for i in range(ProjectConfig.numScreenshots):
+        vstate = bpy.get_vstate(ProjectConfig.widthOfScreenshot)
         with open(os.path.join(outputDir, f'{i:02}.png'), 'wb') as img:
             vstate['img'].save(img, format="PNG")
         time.sleep(1)
