fcbbe583cbd03ee74b6537bffa38d0098befe1c9
Stefan Huber <junktogo@web.de>
2018-08-07 18:01:56 +0200

    Introduce type hints, docstrings, local imports and static variables

--- BeamNG/SimpleBeamNG/main.py
+++ BeamNG/SimpleBeamNG/main.py
@@ -1,36 +1,71 @@
 import os
-import time
 from configparser import ConfigParser
+from typing import Union, Any, Optional
 
-import beamngpy
+
+def static_vars(**kwargs):
+    """
+    Decorator hack for introducing local static variables.
+    :param kwargs: The declarations of the static variables like "foo=42".
+    :return: The decorated function.
+    """
+    def decorate(func):
+        for k in kwargs:
+            setattr(func, k, kwargs[k])
+        return func
+    return decorate
 
 
 class ProjectConfig:
-    currentConfigSection = 'BeamNG'
-    widthOfScreenshot = 800
-    numScreenshots = 10
+    """
+    Contains settings specific to the project but not to the user.
+    """
+    configSection: str = 'BeamNG'
+    widthOfScreenshot: int = 800
+    numRequests: int = 10
+    delayBeforeFirst: int = 3
+    delay: int = 1
 
 
-config = ConfigParser()
-dirname = os.path.abspath(os.path.dirname(__file__))  # NOTE When inserting this line into the next, it may fail
-config.read(os.path.join(dirname, '..', 'BeamNG.ini'))
+def read_user_config() -> ConfigParser:
+    """
+    Returns the user specific configurations.
+    :return: The user specific settings.
+    """
+    config: ConfigParser = ConfigParser()
+    config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'BeamNG.ini'))
+    return config
 
-userpath = config.get(ProjectConfig.currentConfigSection, 'userpath')
-binary = config.get(ProjectConfig.currentConfigSection, 'x64_binary')
-with beamngpy.beamng.BeamNGPy('localhost', 64256, userpath=userpath,
-                              binary=binary, console=True) as bpy:
-    bpy.load_scenario('levels/west_coast_usa/scenarios/basic_scenario.json')
-    bpy.start_scenario()  # NOTE This method blocks until the restart button is pressed
-    bpy.hide_hud()
-    bpy.vcontrol({
-        'throttle': 1.0,
-        'steering': 0.5,
-        'gear': 1
-    })
 
-    outputDir = config.get(ProjectConfig.currentConfigSection, 'output_dir')
-    for i in range(ProjectConfig.numScreenshots):
-        vstate = bpy.get_vstate(ProjectConfig.widthOfScreenshot)
-        with open(os.path.join(outputDir, f'{i:02}.png'), 'wb') as img:
-            vstate['img'].save(img, format="PNG")
-        time.sleep(1)
+@static_vars(config=read_user_config())
+def get_user_config(option: str) -> object:
+    """
+    Returns the user specific setting.
+    :param option: The option to retrieve.
+    :return: The set value of the option.
+    """
+    return get_user_config.config.get(ProjectConfig.configSection, option)
+
+
+if __name__ == '__main__':
+    from beamngpy import BeamNGPy
+    import time
+    userpath: Union[Optional[object], Any] = get_user_config('userpath')
+    binary: Union[Optional[object], Any] = get_user_config('x64_binary')
+    with BeamNGPy('localhost', 64256, userpath=userpath, binary=binary, console=True) as bpy:
+        bpy.load_scenario('levels/west_coast_usa/scenarios/basic_scenario.json')
+        bpy.start_scenario()  # NOTE This method blocks until the restart button is pressed
+        bpy.hide_hud()
+        bpy.vcontrol({
+            'throttle': 1.0,
+            'steering': 0.5,
+            'gear': 1
+        })
+
+        output_dir: Union[Optional[object], Any] = get_user_config('output_dir')
+        time.sleep(ProjectConfig.delayBeforeFirst)
+        for i in range(ProjectConfig.numRequests):
+            vstate: object = bpy.get_vstate(ProjectConfig.widthOfScreenshot)
+            with open(os.path.join(output_dir, f'{i:02}.png'), 'wb') as img:
+                vstate['img'].save(img, format="PNG")
+            time.sleep(ProjectConfig.delay)
