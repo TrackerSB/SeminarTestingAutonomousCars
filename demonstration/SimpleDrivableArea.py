836de1b3a24b39611a0f44fd5f51e511b7823288
Stefan Huber <stefan.huber.niedling@outlook.com>
2018-11-10 11:16:54 +0100

    demonstration: Animate ego vehicle

--- demonstration/SimpleDrivableArea.py
+++ demonstration/SimpleDrivableArea.py
@@ -1,28 +1,22 @@
 import os
-from time import sleep
-from typing import Tuple, Any, List
+from functools import partial
+from typing import Tuple, List, Optional
 from warnings import warn
 
-from commonroad.common.util import Interval
-from commonroad.prediction.prediction import Occupancy
-import numpy as np
-
+import matplotlib.patches as pltpat
 import matplotlib.pyplot as plt
+import numpy as np
 from commonroad.common.file_reader import CommonRoadFileReader
 from commonroad.geometry.shape import Rectangle, Shape, Polygon
-from commonroad.planning.planning_problem import PlanningProblemSet
+from commonroad.planning.planning_problem import PlanningProblemSet, PlanningProblem
 from commonroad.scenario.obstacle import DynamicObstacle
 from commonroad.scenario.scenario import Scenario
+from commonroad.scenario.trajectory import State
 from commonroad.visualization.draw_dispatch_cr import draw_object
-from matplotlib.animation import FuncAnimation, ArtistAnimation
+from matplotlib.animation import ArtistAnimation
 from matplotlib.artist import Artist
-import matplotlib.patches as pltpat
-from matplotlib.collections import PathCollection
-from matplotlib.lines import Line2D
 
-shape_params = {
-}
-draw_params = {
+draw_object_part = partial(draw_object, draw_params={
     'time_begin': 0,
     'antialiased': True,
     'scenario': {
@@ -30,7 +24,7 @@ draw_params = {
             'occupancy': {
                 'draw_occupancies': 1
             },
-            'shape': shape_params,
+            'shape': {},
             'draw_shape': True,
             'draw_icon': True,
             'draw_bounding_box': True,
@@ -39,15 +33,15 @@ draw_params = {
             'zorder': 20
         }
     }
-}
+})
 
 
-def convert_dynamic_obstacle_to_artist(obstacle: DynamicObstacle, timestep: int):
+def convert_to_artist(obstacle: DynamicObstacle, timestep: int) -> Artist:
     shape: Shape = obstacle.occupancy_at_time(timestep).shape
     artist: Artist = None
     if isinstance(shape, Rectangle):
-        left, bottom = shape.center/2
-        angle = obstacle.initial_state.orientation * 360 / 2 / np.pi
+        left, bottom = shape.center / 2
+        angle: float = obstacle.initial_state.orientation * 360 / 2 / np.pi
         artist = plt.gca().add_patch(pltpat.Rectangle((left, bottom), shape.length, shape.width, angle=angle))
     elif isinstance(shape, Polygon):
         artist = plt.gca().add_patch(pltpat.Polygon(np.array(shape.vertices)))
@@ -56,29 +50,61 @@ def convert_dynamic_obstacle_to_artist(obstacle: DynamicObstacle, timestep: int)
     return artist
 
 
-def main():
-    figure = plt.figure()
-    file_path: os.path = os.path.join(os.getcwd(), 'scenarios/DEU_B471-1_1_T-1/DEU_B471-1_1_T-1.xml')
-    scenario: Scenario = CommonRoadFileReader(file_path).open()[0]
-    ego_vehicle: DynamicObstacle = scenario.dynamic_obstacles[1]
-
-    # Draw lanes
-    draw_object(scenario.lanelet_network, draw_params=draw_params, plot_limits=[-200, 250, -100, 100])
-
-    # Draw static obstacles
-    for obstacle in scenario.static_obstacles:
-        draw_object(obstacle, draw_params=draw_params)
-
-    # Animate dynamic obstacles
+def createDynamicObstaclesArtists(dynamic_obstacles: List[DynamicObstacle]) -> List[List[Artist]]:
     artists: List[List[Artist]] = []
     for timestep in range(0, 51):
         frame = []
-        for obstacle in scenario.dynamic_obstacles:
-            frame.append(convert_dynamic_obstacle_to_artist(obstacle, timestep))
+        for obstacle in dynamic_obstacles:
+            frame.append(convert_to_artist(obstacle, timestep))
             # frame.append(draw_object(obstacle.occupancy_at_time(timestep)))
         artists.append(frame)
 
-    ani = ArtistAnimation(figure, artists, blit=True, interval=100)  # NOTE The assignment is needed to force execution
+    return artists
+
+
+def extendFramesWithPrediction(initial_state: State, frames: List[List[Artist]], delay_time_step_sec: float) \
+        -> List[List[Artist]]:
+    draw_object_part(initial_state)
+    current_state: State = initial_state
+    for frame in frames:
+        state_point: pltpat.Circle = pltpat.Circle(current_state.position, 2)
+        frame.append(plt.gca().add_patch(state_point))
+        delta_x: float = np.cos(current_state.orientation) * current_state.velocity * delay_time_step_sec
+        delta_y: float = np.sin(current_state.orientation) * current_state.velocity * delay_time_step_sec
+
+        translation: np.ndarray = np.array([delta_x, delta_y])
+        current_state = current_state.translate_rotate(translation, 0)
+
+    return frames
+
+
+def main() -> None:
+    figure = plt.figure()
+    file_path: os.path = os.path.join(os.getcwd(), 'scenarios/DEU_B471-1_1_T-1/DEU_B471-1_1_T-1.xml')
+    common_road_input: Tuple[Scenario, PlanningProblemSet] = CommonRoadFileReader(file_path).open()
+    scenario: Scenario = common_road_input[0]
+    planning_problem: Optional[PlanningProblem] = common_road_input[1].planning_problem_dict.get(800)
+
+    # Draw goal region
+    draw_object_part(planning_problem.goal)
+
+    # Draw lanes
+    draw_object_part(scenario.lanelet_network, plot_limits=[-200, 250, -100, 100])
+
+    # Draw static obstacles
+    for obstacle in scenario.static_obstacles:
+        draw_object_part(obstacle)
+
+    # Generates frames of dynamic obstacles
+    frames: List[List[Artist]] = createDynamicObstaclesArtists(scenario.dynamic_obstacles)
+
+    # Add predictions of ego vehicle to the frames
+    extendFramesWithPrediction(planning_problem.initial_state, frames, scenario.dt)
+
+    # Add an animation of the frames
+    # NOTE The assignment is needed to force execution
+    ani = ArtistAnimation(figure, frames, blit=True, interval=int(round(scenario.dt * 1000)), repeat=True)
+
     plt.gca().set_aspect('equal')
     plt.show()
 
