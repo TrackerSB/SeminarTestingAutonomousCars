79378db2e3484817a6c2b03310a0edccf49b1e50
TrackerSB <stefan.huber.niedling@outlook.com>
2018-12-01 09:57:23 +0100

    demonstration: Remove unused common.py

--- demonstration/common.py
+++
@@ -1,7 +0,0 @@
-import os
-from typing import Tuple, Optional, List
-
-import numpy as np
-from commonroad.common.file_reader import CommonRoadFileReader
-from commonroad.planning.planning_problem import PlanningProblem
-from commonroad.scenario.scenario import Scenario
