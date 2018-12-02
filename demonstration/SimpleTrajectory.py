997f1b300fffc6af3a1bab9e451cb66c2172948e
TrackerSB <stefan.huber.niedling@outlook.com>
2018-12-02 16:02:04 +0100

    demonstration: Add project generating a simple trajectory

---
+++ demonstration/SimpleTrajectory.py
@@ -0,0 +1,32 @@
+import datetime
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+from shapely.geometry import MultiPolygon
+
+from common import load_scenario, flatten_dict_values
+from common.draw import DrawHelp
+from common.generation import GenerationHelp
+
+
+def main() -> None:
+    scenario, planning_problem = load_scenario('scenarios/DEU_B471-1_1_T-1/DEU_B471-1_1_T-1.xml')
+
+    plt.figure(figsize=(25, 10))
+
+    DrawHelp.draw(DrawHelp.convert_to_drawable(scenario))
+    DrawHelp.draw(DrawHelp.convert_to_drawable(planning_problem.initial_state))
+
+    start_time: datetime = datetime.now()
+    prediction, valid_converted = GenerationHelp.generate_trajectory(scenario, planning_problem, 70)
+    print("Generation took " + str(datetime.now() - start_time))
+
+    for converted in valid_converted:
+        DrawHelp.draw(converted)
+
+    plt.gca().set_aspect('equal')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
