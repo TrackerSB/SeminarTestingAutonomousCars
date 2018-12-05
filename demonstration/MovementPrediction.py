0239aa5d83c334140aa7e1de8a0b94505b371a5e
Stefan Huber <stefan.huber.niedling@outlook.com>
2018-12-05 18:18:30 +0100

    demonstration: Add first version PRM implementation

---
+++ demonstration/MovementPrediction.py
@@ -0,0 +1,55 @@
+import datetime
+from datetime import datetime
+from typing import List, Dict, Tuple, Optional
+
+import matplotlib.pyplot as plt
+from commonroad.common.util import AngleInterval
+from commonroad.geometry.shape import Rectangle
+from commonroad.scenario.trajectory import State
+
+from common import load_scenario, drawable_types, flatten_dict_values
+from common.draw import DrawHelp
+from common.generation import GenerationHelp
+from common.prm import dijkstra_search
+
+
+def main() -> None:
+    scenario, planning_problem = load_scenario('scenarios/DEU_B471-1_1_T-1/DEU_B471-1_1_T-1.xml')
+
+    plt.figure(figsize=(25, 10))
+
+    DrawHelp.draw(DrawHelp.convert_to_drawable(scenario))
+    DrawHelp.draw(DrawHelp.convert_to_drawable(planning_problem.initial_state))
+
+    start_time: datetime = datetime.now()
+    generation_result: Tuple[Dict[int, List[Tuple[State, drawable_types]]], int] \
+        = GenerationHelp.generate_states(scenario, planning_problem, 15)
+    # NOTE The value 50 is taken from the commonroad file
+    num_states_processed: int = generation_result[1]
+    valid_converted: Dict[int, List[Tuple[State, drawable_types]]] = generation_result[0]
+    valid_states: List[State] = list(map(lambda t: t[0], flatten_dict_values(valid_converted)))
+    print("Processed " + str(num_states_processed) + " states in " + str(datetime.now() - start_time))
+
+    # FIXME Properly define goal states
+    goal_states: List[State] = []
+    for goal in planning_problem.goal.state_list:
+        if isinstance(goal.position, Rectangle):
+            goal.position = goal.position.center
+            goal.orientation = (goal.orientation.end + goal.orientation.start) / 2
+        goal.time_step = 16
+        goal_states.append(goal)
+    search_result: Optional[Tuple[List[State], float]] = dijkstra_search(
+        planning_problem.initial_state, goal_states, valid_states)
+
+    if search_result:
+        for state in search_result[0]:
+            DrawHelp.draw(DrawHelp.convert_to_drawable(state))
+    else:
+        print("Could not find path to goal")
+
+    plt.gca().set_aspect('equal')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
---
+++ demonstration/common/prm.py
@@ -0,0 +1,60 @@
+from typing import List, Tuple, Optional
+
+from commonroad.scenario.trajectory import State
+from numpy.core.umath import subtract
+from numpy.linalg import norm
+
+
+class DijkstraNode:
+    def __init__(self, state: State, is_goal_node: bool = False, costs: float = -1, previous=None):
+        """
+        Represents a node in a dijkstra search.
+        :param state: The commonroad state this node represents.
+        :param is_goal_node: If set to True this node represents a goal node.
+        :param costs: The current costs to reach this node from the start node. Costs of -1 represent unknown costs.
+        :param previous: The predecessor of this node in the Dijkstra search graph.
+        """
+        self.state = state
+        self.is_goal_node = is_goal_node
+        self.costs = costs
+        self.previous = previous
+
+
+def dijkstra_search(start_state: State, goal_states: List[State], states: List[State]) \
+        -> Optional[Tuple[List[State], float]]:
+    # NOTE Per definition there is an edge from state A to state B if and only if A.time_step == B.time_step - 1
+    if start_state in states:
+        states.remove(start_state)
+
+    # NOTE White and grey nodes refer to their previous node (second element in tuple)
+    white_nodes: List[DijkstraNode] = []
+    grey_nodes: List[DijkstraNode] = [DijkstraNode(start_state, costs=0)]
+    black_nodes: List[DijkstraNode] = []
+    for goal_state in goal_states:
+        if goal_state in states:
+            states.remove(goal_state)
+        black_nodes.append(DijkstraNode(goal_state, is_goal_node=True))
+    for state in states:
+        black_nodes.append(DijkstraNode(state))
+
+    next_white: DijkstraNode = DijkstraNode(None)
+    while grey_nodes != [] and not next_white.is_goal_node:
+        next_white: DijkstraNode = min(grey_nodes, key=lambda gp: gp.costs)
+        grey_nodes.remove(next_white)
+        white_nodes.append(next_white)
+        for black in black_nodes:
+            if next_white.state.time_step == black.state.time_step - 1:
+                # FIXME Costs should be influenced by the orientation difference
+                costs: float = black.costs + norm(next_white.state.position - black.state.position)
+                grey_nodes.append(
+                    DijkstraNode(black.state, is_goal_node=black.is_goal_node, costs=costs, previous=next_white))
+                black_nodes.remove(black)
+
+    if next_white.is_goal_node:
+        costs: float = next_white.costs
+        path: List[State] = []
+        current: DijkstraNode = next_white
+        while current.previous:
+            path.append(current.state)
+            current = current.previous
+        return path, costs
