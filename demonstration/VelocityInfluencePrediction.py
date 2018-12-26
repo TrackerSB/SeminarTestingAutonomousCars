69d3df011e357db8a8e4095fa53bf5e721e78c6c
Stefan Huber <stefan.huber.niedling@outlook.com>
2018-12-26 15:25:46 +0100

    demonstration: Improve the plots of velocity influence prediction

--- demonstration/VelocityInfluencePrediction.py
+++ demonstration/VelocityInfluencePrediction.py
@@ -1,6 +1,6 @@
 from copy import copy
 from datetime import datetime
-from typing import List
+from typing import List, Dict
 
 import matplotlib.pyplot as plt
 from shapely.geometry import MultiPolygon
@@ -22,13 +22,21 @@ def main() -> None:
         for obs in scenario.dynamic_obstacles:
             fixed_drawables.append(DrawHelp.convert_to_drawable([obs.occupancy_at_time(time_step)]))
 
-    for velocity in range(18, 50):
-        planning_problem.initial_state.velocity = velocity
-        plt.figure(figsize=(19.20, 10.80), dpi=100)
+    min_velocity: int = 15
+    max_velocity: int = 50
+    drivable_areas: Dict[int, float] = {}
+    for velocity in range(min_velocity, max_velocity + 1):
+        # Prepare figure and subplots
+        main_fig, (scenario_fig, area_fig) = plt.subplots(nrows=1, ncols=2, figsize=(19.20, 10.80), dpi=100)
+        main_fig.suptitle("Influence of velocity to drivable area", fontsize=32)
+        plt.sca(scenario_fig)
 
+        # Draw artists to be shown always
         for fixed in fixed_drawables:
             DrawHelp.draw(copy(fixed))
 
+        # Generate and draw states resulting from the current velocity of the ego vehicle
+        planning_problem.initial_state.velocity = velocity
         start_time: datetime = datetime.now()
         valid_converted, num_states_processed \
             = GenerationHelp.generate_states(scenario, MyState(planning_problem.initial_state), num_time_steps)
@@ -41,10 +49,23 @@ def main() -> None:
 
         union: MultiPolygon = DrawHelp.union_to_polygon(list(map(lambda v: v.drawable, all_states)))
         DrawHelp.draw(union)
-        print("Drivable area: " + str(union.area))
+        drivable_areas[velocity] = union.area
 
-        plt.gca().set_aspect('equal')
-        plt.axis([50, 200, 0, 100])
+        scenario_fig.set_aspect('equal')
+        scenario_fig.set_xlim([50, 200])
+        scenario_fig.set_ylim([0, 100])
+        scenario_fig.set_xlabel("position [m]")
+        scenario_fig.set_ylabel("position [m]")
+
+        # Draw changes of drivable area
+        area_fig.set_xlim([min_velocity, max_velocity])
+        area_fig.set_ylim([0, 60])
+        area_fig.plot(drivable_areas.keys(), drivable_areas.values(), 'x',
+                      drivable_areas.keys(), drivable_areas.values(), '-')
+        area_fig.set_xlabel("velocity [m/s]")
+        area_fig.set_ylabel("area [m²]")
+
+        # Save the figure
         plt.savefig("out_velocity_" + str(velocity))
         # plt.close()  # FIXME Causes EOFErrors
 
