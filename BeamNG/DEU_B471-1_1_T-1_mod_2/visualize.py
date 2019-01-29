94e7a4c868fbea38e0e54ef774d118554f7d93f6
TrackerSB <stefan.huber.niedling@outlook.com>
2019-01-29 13:03:34 +0100

    demonstration: Add setup for simulating commonroad scenario

--- BeamNG/DEU_B471-1_1_T-1_mod_2/visualize.py
+++ BeamNG/DEU_B471-1_1_T-1_mod_2/visualize.py
@@ -1,26 +1,32 @@
-from beamngpy import BeamNGpy, Scenario, Vehicle, Road
-
-
 # Visualize DEU_B471-1_1_T-1_mod_2
+from beamngpy import BeamNGpy, Scenario, Vehicle, Road, os
 
-z_offset: int = 700  # Make sure to position the car outside the buildings
-rot_offset: float = 180
+user_path = 'G:/gitrepos/SeminarTestingAutonomousCars/BeamNG/BeamNGUserpath'
+home_path = 'G:/gitrepos/beamng-research_unlimited/trunk'
+scenario_environment = 'smallgrid'
+scenario_name = 'commonroad'
 
 
 def main() -> None:
     # Setup BeamNG
-    bng = BeamNGpy('localhost', 64256, home='G:/gitrepos/beamng-research_unlimited/trunk',
-          user='G:/gitrepos/SeminarTestingAutonomousCars/BeamNG/BeamNGUserpath')
-    scenario = Scenario('smallgrid', 'commonroad', authors='Stefan Huber',
+    bng = BeamNGpy('localhost', 64256, home=home_path, user=user_path)
+    scenario = Scenario(scenario_environment, scenario_name, authors='Stefan Huber',
                         description='Simple visualization of the modified version of the CommonRoad scenario '
                                     'DEU_B471-1_1_T-1')
 
-    # Add vehicle
-    vehicle = Vehicle('ego_vehicle', model='etk800', licence='EGO', color='White')
-    scenario.add_vehicle(vehicle, pos=(0, 0, 0), rot=(0, 0, 0))
+    # Add ego vehicle
+    ego_vehicle = Vehicle('ego_vehicle', model='etk800', licence='EGO', color='White')
+    scenario.add_vehicle(ego_vehicle, pos=(0, 0, 0.212443), rot=(1, 0, 0))
+
+    # Add truck
+    truck = Vehicle('truck', model='tanker', licence='TRUCK', color='Red')
+    scenario.add_vehicle(truck, pos=(50, 0, 1.21453), rot=(1, 0, 0, 0.137056))
+
+    # Add other traffic participant
+    # TODO
 
     # Create road
-    road = Road('AsphaltRoad_variation_02')
+    road = Road('track_editor_H_center')  # FIXME overObjects=True is not supported
     nodes = [
         (0, 0, 0, 5),
         (0, 100, 0, 5),
@@ -30,6 +36,14 @@ def main() -> None:
     scenario.add_road(road)
 
     scenario.make(bng)
+
+    # Manually set the overObjects attribute of all roads to True (Setting this option is currently not supported)
+    prefab_path = os.path.join(user_path, 'levels', scenario_environment, 'scenarios', scenario_name + '.prefab')
+    lines = open(prefab_path, 'r').readlines()
+    for i in range(len(lines)):
+        if 'overObjects' in lines[i]:
+            lines[i] = lines[i].replace('0', '1')
+    open(prefab_path, 'w').writelines(lines)
 
     # Start simulation
     bng.open(launch=True)
